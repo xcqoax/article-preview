d57ce98b94ab62c4fd535e1f28f665cb3749ca31
Gonzalo Peralta <palmer_85@hotmail.com>
2020-09-13 02:11:12 -0500

    subiendo archivo

---
+++ javascript.js
@@ -0,0 +1,7 @@
+let compartir = document.getElementById('btn-compartir')
+let redes_compartir = document.getElementById('redes-sociales-compartir')
+
+compartir.onclick = function(){
+    redes_compartir.classList.toggle('ocultar')
+}
+
